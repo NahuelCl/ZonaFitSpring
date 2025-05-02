a1e71bfc0891bcaa4623c7c460f5ca0869e17213
Nahuel Clauser <clausernahuel@gmail.com>
2025-05-02 17:05:45 -0300

    Adding logger

--- src/main/java/gm/zona_fit/ZonaFitApplication.java
+++ src/main/java/gm/zona_fit/ZonaFitApplication.java
@@ -1,12 +1,23 @@
 package gm.zona_fit;
 
+import gm.zona_fit.service.IClienteService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class ZonaFitApplication {
 
+	@Autowired
+	private IClienteService clienteService;
+
+	private static final Logger logger = LoggerFactory.getLogger(ZonaFitApplication.class);
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(ZonaFitApplication.class, args);
 	}
 
