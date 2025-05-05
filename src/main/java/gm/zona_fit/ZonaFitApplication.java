8bdfbda2d3a18879facd67f053d298ff13b694cb
Nahuel Clauser <clausernahuel@gmail.com>
2025-05-05 16:40:50 -0300

    Funcionalidad menu

--- src/main/java/gm/zona_fit/ZonaFitApplication.java
+++ src/main/java/gm/zona_fit/ZonaFitApplication.java
@@ -1,5 +1,6 @@
 package gm.zona_fit;
 
+import gm.zona_fit.modelo.Cliente;
 import gm.zona_fit.service.IClienteService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -37,7 +40,7 @@ public class ZonaFitApplication implements CommandLineRunner {
 		var consola = new Scanner(System.in);
 		while(!salir){
 			var opcion = mostrarMenu(consola);
-			//salir = ejecutarOpciones(consola, opcion);
+			salir = ejecutarOpciones(consola, opcion);
 			logger.info(" ");
 		}
 	}
@@ -52,5 +55,79 @@ public class ZonaFitApplication implements CommandLineRunner {
 				Elige una opcion:\s
 				""");
 		return  Integer.parseInt(consola.nextLine());
+	}
+
+	private boolean ejecutarOpciones(Scanner consola, int opcion){
+		var salir = false;
+		switch (opcion){
+			case 1 -> {
+				logger.info(nl + "--- Listado de Clientes ---" + nl);
+				List<Cliente> clientes = clienteService.listarClientes();
+				clientes.forEach(cliente -> logger.info(cliente.toString() + nl));
+			}
+			case 2 -> {
+				logger.info(nl + "--- Buscar Cliente ---" + nl);
+				var idCliente = Integer.parseInt(consola.nextLine());
+				Cliente c = clienteService.buscarClientePorId(idCliente);
+				if (c == null){
+					logger.info("Cliente no encontrado"+ nl);
+				}else{
+					logger.info("Cliente encontrado:" + c + nl);
+				}
+			}
+			case 3 ->{
+				logger.info(nl + "--- Agregar Cliente ---" + nl);
+				logger.info("Nombre: ");
+				var nombre = consola.nextLine();
+				logger.info("Apellido:");
+				var apellido = consola.nextLine();
+				logger.info("Membresia: ");
+				var membresia = Integer.parseInt(consola.nextLine());
+				var cliente = new Cliente();
+				cliente.setNombre(nombre);
+				cliente.setApellido(apellido);
+				cliente.setMembresia(membresia);
+				clienteService.guardarCliente(cliente);
+				logger.info(nl + "Cliente agregado:" + cliente + nl);
+			}
+			case 4 ->{
+				logger.info(nl + "--- Modificar Cliente ---" + nl);
+				logger.info("Id cliente:");
+				var idCliente = Integer.parseInt(consola.nextLine());
+				Cliente c = clienteService.buscarClientePorId(idCliente);
+				if(c != null){
+					logger.info("Nombre:");
+					var nombre = consola.nextLine();
+					logger.info("Apellido:");
+					var apellido = consola.nextLine();
+					logger.info("Membresia:");
+					var membresia = Integer.parseInt(consola.nextLine());
+					c.setNombre(nombre);
+					c.setApellido(apellido);
+					c.setMembresia(membresia);
+					clienteService.guardarCliente(c);
+					logger.info("Cliente modificado" + c + nl);
+				}else{
+					logger.info("Cliente no encontrado" + nl);
+				}
+			}
+			case 5 ->{
+				logger.info(nl + "--- Eliminar Cliente ---" + nl);
+				logger.info("Id cliente:");
+				var idCliente = Integer.parseInt(consola.nextLine());
+				Cliente c = clienteService.buscarClientePorId(idCliente);
+				if(c != null){
+					clienteService.eliminarCliente(c);
+				}else{
+					logger.info("El cliente no existe" + nl);
+				}
+			}
+			case 6 ->{
+				logger.info("Hasta pronto" + nl);
+				salir = true;
+			}
+			default -> logger.info("Opcion incorrecta" + nl);
+		}
+		return salir;
 	}
 }
