7020c3179a569208255e4c2ea0842e897943ab15
Nahuel Clauser <clausernahuel@gmail.com>
2025-05-21 17:11:21 -0300

    All working fine

--- src/main/java/gm/zona_fit/gui/ZonaFitForm.java
+++ src/main/java/gm/zona_fit/gui/ZonaFitForm.java
@@ -1,5 +1,6 @@
 package gm.zona_fit.gui;
 
+import gm.zona_fit.modelo.Cliente;
 import gm.zona_fit.service.ClienteService;
 import gm.zona_fit.service.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,10 @@ import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 @Component
 
@@ -14,8 +19,15 @@ public class ZonaFitForm extends JFrame{
     private JPanel panelPrincipal;
     private JTable clientesTabla;
     private JScrollPane scrollPane;
+    private JTextField nombreTexto;
+    private JTextField apellidoTexto;
+    private JTextField membresiaNumero;
+    private JButton saveButton;
+    private JButton deleteButton;
+    private JButton clearButton;
     IClienteService clienteService;
     private DefaultTableModel tablaModeloClientes;
+    private Integer idCliente;
 
     @Autowired
     public ZonaFitForm(ClienteService clienteServicio){
@@ -23,6 +35,16 @@ public class ZonaFitForm extends JFrame{
         createUIComponents();
         iniciarForma();
         SwingUtilities.invokeLater(this::listarClientes);
+        saveButton.addActionListener( e ->guardarCliente());
+        clientesTabla.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                cargarClienteSeleccionado();
+            }
+        });
+        deleteButton.addActionListener(e -> eliminarCliente());
+        clearButton.addActionListener(e -> limpiarFormulario());
     }
 
     private void iniciarForma(){
@@ -33,16 +55,22 @@ public class ZonaFitForm extends JFrame{
         setVisible(true);
     }
 
-
     private void createUIComponents() {
         // TODO: place custom component creation code here
         String[] cabeceros = {"Id", "Nombre", "Apellido", "Membresia"};
-        this.tablaModeloClientes = new DefaultTableModel(cabeceros, 0);
+        this.tablaModeloClientes = new DefaultTableModel(cabeceros, 0){
+            @Override
+            public boolean isCellEditable(int row, int cell){
+                return false;
+            }
+        };
         this.clientesTabla = new JTable(this.tablaModeloClientes);
 
+        this.clientesTabla.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         if (scrollPane != null) {
             scrollPane.setViewportView(clientesTabla);
         }
+
     }
 
     private void listarClientes(){
@@ -58,5 +86,76 @@ public class ZonaFitForm extends JFrame{
             this.tablaModeloClientes.addRow(renglonCliente);
         });
     }
+
+    private void guardarCliente(){
+        if(nombreTexto.getText().equals("")){
+            mostrarMensaje("Proporciona un nombre");
+            nombreTexto.requestFocusInWindow();
+            return;
+        }
+        if(membresiaNumero.getText().equals("")){
+            mostrarMensaje("Proporciona membresia");
+            membresiaNumero.requestFocusInWindow();
+            return;
+        }
+
+        var nombre = nombreTexto.getText();
+        var apellido = apellidoTexto.getText();
+        var membresia = Integer.parseInt(membresiaNumero.getText());
+        var cliente = new Cliente(this.idCliente,nombre,apellido,membresia);
+        this.clienteService.guardarCliente(cliente);
+        if(this.idCliente == null){
+            mostrarMensaje("Se agrego el nuevo cliente");
+        }else {
+            mostrarMensaje("Se actualiizo el cliente");
+        }
+
+        limpiarFormulario();
+        listarClientes();
+
+    }
+
+    private void cargarClienteSeleccionado(){
+        var renglon = clientesTabla.getSelectedRow();
+        if (renglon != -1){
+            var id = clientesTabla.getModel().getValueAt(renglon, 0).toString();
+            this.idCliente = Integer.parseInt(id);
+            var nombre = clientesTabla.getModel().getValueAt(renglon, 1).toString();
+            this.nombreTexto.setText(nombre);
+            var apellido = clientesTabla.getModel().getValueAt(renglon, 2).toString();
+            this.apellidoTexto.setText(apellido);
+            var membresia = clientesTabla.getModel().getValueAt(renglon, 3).toString();
+            this.membresiaNumero.setText(membresia);
+        }
+    }
+
+    private void eliminarCliente(){
+        var renglon = clientesTabla.getSelectedRow();
+        if (renglon != -1){
+            var idClienteStr = clientesTabla.getModel().getValueAt(renglon,0).toString();
+            this.idCliente = Integer.parseInt(idClienteStr);
+            var cliente = new Cliente();
+            cliente.setId(this.idCliente);
+            clienteService.eliminarCliente(cliente);
+            mostrarMensaje("Cliente con id: "+ this.idCliente +" eliminado.");
+            limpiarFormulario();
+            listarClientes();
+        }
+    }
+
+    private void mostrarMensaje(String mensaje){
+        JOptionPane.showMessageDialog(this,mensaje);
+    }
+
+    private void limpiarFormulario(){
+        nombreTexto.setText("");
+        apellidoTexto.setText("");
+        membresiaNumero.setText("");
+        this.idCliente = null;
+        this.clientesTabla.getSelectionModel().clearSelection();
+    }
+
+
+
 }
 
